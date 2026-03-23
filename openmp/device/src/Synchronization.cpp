1b4bedf406b34d5c560afc2ebbc84fa618989145
Miguel Cárdenas <miguelecsx@gmail.com>
2026-03-23 14:03:00 -0500

    [OpenMP][Device] Fix SPIR-V workgroup barrier for Metal GPU target
    - Replace __builtin_trap() stubs in the SPIR-V namedBarrier path
    - namedBarrierInit() is a no-op: hardware workgroup barriers need no SW init
    - namedBarrier() delegates to synchronize::threads(seq_cst) which maps to
      OpControlBarrier(Workgroup, Workgroup, AcquireRelease) on SPIR-V
    - Unblocks GENERIC mode kernels and the OpenMP worker state machine on Metal
    
    Signed-off-by: Miguel Cárdenas <miguelecsx@gmail.com>

--- openmp/device/src/Synchronization.cpp
+++ openmp/device/src/Synchronization.cpp
@@ -181,8 +181,9 @@ void setCriticalLock(omp_lock_t *Lock) { setLock(Lock); }
 ///}
 
 #if defined(__SPIRV__)
-void namedBarrierInit() { __builtin_trap(); } // TODO
-void namedBarrier() { __builtin_trap(); }     // TODO
+// SPIR-V uses hardware workgroup barriers; no software counter needed.
+void namedBarrierInit() {}
+void namedBarrier() { synchronize::threads(atomic::seq_cst); }
 
 void unsetLock(omp_lock_t *Lock) {
   atomic::store((int32_t *)Lock, 0, atomic::seq_cst);
